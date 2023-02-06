06262140479262ae72a3fefbe1b970c2bf8fbba6
Jaeho Lee 이재호 <dlwogh9344@khu.ac.kr>
2023-02-06 22:47:47 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cd django && docker build -t test/django . && cd ../
-cd nginx && docker build -t test/nginx . && cd ../
+cd django && docker build -t seraph/django . && cd ../
+cd nginx && docker build -t seraph/nginx . && cd ../
 docker-compose up --build -d
